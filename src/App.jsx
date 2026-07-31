6c251038ec395aa35986561a19f895abc254d3bc
netwrok1001 <prakharofficial6@gmail.com>
2026-07-31 15:32:39 +0530

    solved-footer-rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
 import './App.css'
 import Navbar from './Components/navbar'
 import Hero from './Components/Hero'
@@ -14,31 +14,46 @@ import Feedback from './pages/Feedback'
 import Alumini from './pages/Alumini'
 import Modal from './Components/Modal'
 import Enroll from './pages/Enroll'
+import ScrollToTop from './Components/ScrollToTop'
+import PageTransition from './Components/PageTransition'
+
+function AppContent() {
+  const location = useLocation()
+
+  return (
+    <>
+      <ScrollToTop />
+      <Navbar />
+      <Modal />
+
+      <PageTransition key={location.pathname}>
+        <Routes location={location}>
+          <Route path="/" element={
+            <>
+              <Hero />
+              <Courses />
+              <About />
+              <Contact />
+              <Footer />
+            </>
+          } />
+          <Route path="/gallery" element={<PhotoGallery />} />
+          <Route path="/library" element={<Library />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/faculty" element={<Faculty />} />
+          <Route path="/feedback" element={<Feedback />} />
+          <Route path="/alumini" element={<Alumini />} />
+          <Route path="/enroll" element={<Enroll />} />
+        </Routes>
+      </PageTransition>
+    </>
+  )
+}
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Modal />
-
-      <Routes>
-        <Route path="/" element={
-          <>
-            <Hero />
-            <Courses />
-            <About />
-            <Contact />
-            <Footer />
-          </>
-        } />
-        <Route path="/gallery" element={<PhotoGallery />} />
-        <Route path="/library" element={<Library />} />
-        <Route path="/news" element={<News/>} />
-        <Route path="/faculty" element={<Faculty />} />
-        <Route path="/feedback" element={<Feedback />} />
-        <Route path="/alumini" element={<Alumini />} />
-        <Route path="/enroll" element={<Enroll />} />
-      </Routes>
+      <AppContent />
     </Router>
   )
 }
--- src/pages/Photo_gallery.jsx
+++ src/pages/Photo_gallery.jsx
@@ -66,5 +66,6 @@ export default function PhotoGallery() {
             </div>
             <Footer />
         </section>
+        
     );
 }
